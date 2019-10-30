b7777b7417765580f3c7c6c99831ecd6bd68501b
Aarón Mejía Alfaro <aaron.mejia@gestionysistemas.com>
2019-10-30 02:25:34 -0500

    Despliegue exitoso de Azure Function de Topic

--- FacturaFunctionApp/AzureServiceHandler.cs
+++ FacturaFunctionApp/AzureServiceHandler.cs
@@ -1,15 +1,18 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace FacturaFunctionApp
 {
     public static class AzureServiceHandler
     {
-        [FunctionName("FacturaFunctionApp")]
-        public static void Run([ServiceBusTrigger("pedidosender", "azfunction", Connection = "AzureWebJobsStorage")]string mySbMsg, ILogger log)
+        [FunctionName("FacturaFunctionAppV2")]
+        public static void Run([ServiceBusTrigger("pedidosender", "azfunction", Connection = "ServiceBusConnectionString")]string mySbMsg, ILogger log)
         {
+            log.LogInformation($"COMENZÓ EL TRIGGER {new DateTime().ToString("dd/MM/yyyy HH:mm:ss")}");
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            log.LogInformation($"TERMINÓ EL TRIGGER {new DateTime().ToString("dd/MM/yyyy HH:mm:ss")}");
         }
     }
 }
